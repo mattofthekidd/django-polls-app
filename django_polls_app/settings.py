88015948fe7b0c79c0521342c313e9bb77a6c987
mattofthekidd <mattofthekidd@gmail.com>
2017-04-14 02:15:30 +0000

    Still on the second page of the tutorial. Doing work in the admin site, wanted a backup.

--- django_polls_app/settings.py
+++ django_polls_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
